81863cea50fff7426df13216fc967627a2df9373
asharifi90 <asharifibb@gmail.com>
2024-04-25 18:30:50 +0330

    fill the UniversityType Enum

--- src/main/java/org/alireza/model/UniversityType.java
+++ src/main/java/org/alireza/model/UniversityType.java
@@ -1,4 +1,14 @@
 package org.alireza.model;
 
 public enum UniversityType {
+
+    DOWLATI_ROOZANEH,
+    DOWLATI_SHABANEH,
+    GHEIR_ENTEFAII,
+    PARDIS,
+    ZARFIAT_MAZAD,
+    PAYAM_NOOR,
+    ELMI_KARBORDI,
+    AZAD,
+
 }
