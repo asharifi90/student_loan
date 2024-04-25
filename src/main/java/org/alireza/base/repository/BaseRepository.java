1fc499bf60865c021ee04c78414bb87e6d16b9e0
asharifi90 <asharifibb@gmail.com>
2024-04-25 11:42:15 +0330

    add BaseRepository class that extends from BaseEntity

--- src/main/java/org/alireza/base/repository/BaseRepository.java
+++ src/main/java/org/alireza/base/repository/BaseRepository.java
@@ -1,4 +1,18 @@
 package org.alireza.base.repository;
 
-public interface BaseRepository {
+import org.alireza.base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseRepository<T extends BaseEntity<ID>, ID extends Serializable> {
+
+    T saveOrUpdate(T entity);
+
+    Optional<T> findById(ID id);
+
+    void delete(T entity);
+
+    List<T> findAll();
 }
