e167b4a45179345a348b65296b480c33e2506e78
asharifi90 <asharifibb@gmail.com>
2024-04-25 11:59:25 +0330

    override BaseRepository inteface methods in BaseRepositoryImpl class

--- src/main/java/org/alireza/base/repository/BaseRepositoryImpl.java
+++ src/main/java/org/alireza/base/repository/BaseRepositoryImpl.java
@@ -1,4 +1,53 @@
 package org.alireza.base.repository;
 
-public class BaseRepositoryImpl {
+import lombok.AllArgsConstructor;
+import org.alireza.base.entity.BaseEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+import java.util.Queue;
+
+@AllArgsConstructor
+public abstract class BaseRepositoryImpl <T extends BaseEntity<ID>, ID extends Serializable>
+implements BaseRepository<T, ID>{
+
+    private SessionFactory sessionFactory;
+
+
+    @Override
+    public T saveOrUpdate(T entity) {
+        Session session = sessionFactory.getCurrentSession();
+        if (entity.getId() == null)
+            session.persist(entity);
+        else
+            entity = session.merge(entity);
+        return entity;
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        Session session = sessionFactory.getCurrentSession();
+        return Optional.ofNullable(session.get(getEntityClass(), id));
+    }
+
+    public abstract Class<T> getEntityClass();
+
+    public abstract String getEntity();
+
+    @Override
+    public void delete(T entity) {
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(entity);
+    }
+
+    @Override
+    public List<T> findAll() {
+       Session session = sessionFactory.getCurrentSession();
+        Query<T> query = session.createQuery(String.format("from %s", getEntity()), getEntityClass());
+        return query.getResultList();
+    }
 }
