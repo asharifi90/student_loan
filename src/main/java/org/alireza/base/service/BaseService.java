bc7056fa9287bf65994d979dae69ed75b4f367ee
asharifi90 <asharifibb@gmail.com>
2024-04-25 12:10:28 +0330

    add CRUD methods to BaseService

--- src/main/java/org/alireza/base/service/BaseService.java
+++ src/main/java/org/alireza/base/service/BaseService.java
@@ -1,4 +1,17 @@
 package org.alireza.base.service;
 
-public interface BaseService {
+import org.alireza.base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<T extends BaseEntity<ID>, ID extends Serializable> {
+
+    T saveOrUpdate(T entity);
+
+    T findById(ID id);
+
+    void deleteById(ID id);
+
+    List<T> findAll();
 }
