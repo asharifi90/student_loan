ba961bb96fcd88f185b909f131c933c1a50000a2
asharifi90 <asharifibb@gmail.com>
2024-04-25 20:40:59 +0330

    add lombok annotations

--- src/main/java/org/alireza/model/BankCard.java
+++ src/main/java/org/alireza/model/BankCard.java
@@ -1,10 +1,22 @@
 package org.alireza.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.alireza.base.entity.BaseEntity;
 
 import java.time.LocalDate;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "BankCard")
 public class BankCard extends BaseEntity<Long> {
 
     private String bankName;
