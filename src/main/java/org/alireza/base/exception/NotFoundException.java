869382ea2a15bea8c31190ad40db40aad7e3a395
asharifi90 <asharifibb@gmail.com>
2024-04-25 11:27:45 +0330

    add NotFoundException class that extends from RuntimeException

--- src/main/java/org/alireza/base/exception/NotFoundException.java
+++ src/main/java/org/alireza/base/exception/NotFoundException.java
@@ -1,4 +1,8 @@
 package org.alireza.base.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message){
+        super(message);
+    }
 }
