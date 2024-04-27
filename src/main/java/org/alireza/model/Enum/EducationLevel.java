547c375714edc43de27abb5d0d894b1e2c2753fd
asharifi90 <asharifibb@gmail.com>
2024-04-27 12:37:25 +0330

    add fields

--- src/main/java/org/alireza/model/Enum/EducationLevel.java
+++ src/main/java/org/alireza/model/Enum/EducationLevel.java
@@ -1,4 +1,16 @@
 package org.alireza.model.Enum;
 
 public enum EducationLevel {
+
+    KARDANI,
+
+    KARSHENASI,
+
+    KARSHENASI_ARSHAD,
+
+    DOCTORA_HERFEEI,
+
+    DOCTORA_PEIVASTE,
+
+    DOCTORA_NAPEIVASTE,
 }
