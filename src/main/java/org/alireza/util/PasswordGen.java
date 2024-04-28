0aacc47cce07f47f742a9713df1e63271125e53f
asharifi90 <asharifibb@gmail.com>
2024-04-28 19:57:02 +0330

    add a method to generate a secure password

--- src/main/java/org/alireza/util/PasswordGen.java
+++ src/main/java/org/alireza/util/PasswordGen.java
@@ -1,4 +1,29 @@
 package org.alireza.util;
 
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
+
 public class PasswordGen {
+
+    public static String generateSecurePassword() {
+
+        CharacterRule LCR = new CharacterRule(EnglishCharacterData.LowerCase);
+        LCR.setNumberOfCharacters(2);
+
+        CharacterRule UCR = new CharacterRule(EnglishCharacterData.UpperCase);
+        UCR.setNumberOfCharacters(2);
+
+        CharacterRule DR = new CharacterRule(EnglishCharacterData.Digit);
+        DR.setNumberOfCharacters(2);
+
+        CharacterRule SR = new CharacterRule(EnglishCharacterData.Special);
+        SR.setNumberOfCharacters(2);
+
+        PasswordGenerator passGen = new PasswordGenerator();
+
+        String password = passGen.generatePassword(8, SR, LCR, UCR, DR);
+
+        return password;
+    }
 }
